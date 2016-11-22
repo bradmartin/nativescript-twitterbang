e8fd8bc39a9f6cf6a00023a1f5c17eebb4732c5b
bradmartin <bradwaynemartin@gmail.com>
2016-11-22 12:17:18 -0600

    new license

--- twitterbang.android.ts
+++ twitterbang.android.ts
@@ -1,3 +1,10 @@
+/**************************************************************************************
+ * Made for the {N} community by Brad Martin @BradWayneMartin
+ * https://twitter.com/BradWayneMartin
+ * https://github.com/bradmartin
+ * Pull requests are welcome. Enjoy!
+ *************************************************************************************/
+
 import * as app from 'application';
 import { isAndroid } from 'platform';
 
--- twitterbang.ios.ts
+++ twitterbang.ios.ts
@@ -1,3 +1,10 @@
+/**************************************************************************************
+ * Made for the {N} community by Brad Martin @BradWayneMartin
+ * https://twitter.com/BradWayneMartin
+ * https://github.com/bradmartin
+ * Pull requests are welcome. Enjoy!
+ *************************************************************************************/
+
 export function bang(view) {
     console.log('Not supported on iOS.');
     return;
